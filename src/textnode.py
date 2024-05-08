063bdceeccd7c81ca4ee1970bc80016a35bfbcda
ThachNguyen <phidaotrang@gmail.com>
2024-05-08 16:58:39 +0700

    fix: text to html

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,4 @@
-from htmlnode import HTMLNode, LeafNode
+from htmlnode import LeafNode
 
 text_type_text = "text"
 text_type_bold = "bold"
@@ -35,8 +35,8 @@ def text_node_to_html_node(text_node):
     if text_node.text_type == text_type_italic:
         return LeafNode("i", text_node.text)
     if text_node.text_type == text_type_code:
-        return HTMLNode("code", text_node.text)
+        return LeafNode("code", text_node.text)
     if text_node.text_type == text_type_link:
-        return HTMLNode("a", text_node.text, None, {"href": text_node.url})
+        return LeafNode("a", text_node.text, {"href": text_node.url})
     if text_node.text_type == text_type_image:
-        return HTMLNode("img", "", None, {"src": text_node.url, "alt": text_node.text})
+        return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
